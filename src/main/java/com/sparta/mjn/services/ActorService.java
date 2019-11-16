01f7a9d81bee4fd4e6014c49cc14cd0c761277bf
Andres <ludena.wilson@outlook.com>
2019-11-16 19:40:39 +0000

    Updates: maybe need other plug ins?

--- src/main/java/com/sparta/mjn/services/ActorService.java
+++ src/main/java/com/sparta/mjn/services/ActorService.java
@@ -1,11 +1,23 @@
 package com.sparta.mjn.services;
 
-import javax.persistence.OneToMany;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
+@Stateful
 public class ActorService {
+
+    @PersistenceContext(unitName = "UserPersistenceUnit")
+    private EntityManager em;
 
     public String requestFilms(){
 
         return "films";
     }
+
+    public List getActorList(){
+        return em.createQuery("SELECT first_name, last_name FROM actor").getResultList();
+    }
+
 }
