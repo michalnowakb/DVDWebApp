df9589ec4568204faf2a105c5eb42a9940f0a009
Horia Constantina <hconstantina@spartaglobal.com>
2019-11-17 16:30:15 +0000

    Merge branch 'romaniabranch' of C:\Users\Horia Constantina\DVDWebApp with conflicts.

---
+++ src/main/java/com/sparta/mjn/entities/Actor.java
@@ -0,0 +1,57 @@
+package com.sparta.mjn.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "actor")
+public class Actor
+{
+    @Id
+    @Column(name = "actor_id")
+    private int actorID;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
+
+    public int getActorID() {
+        return actorID;
+    }
+
+    public void setActorID(int actorID) {
+        this.actorID = actorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
