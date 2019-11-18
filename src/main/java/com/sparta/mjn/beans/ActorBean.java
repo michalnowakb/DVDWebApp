7680b4848d91f7b669c40189a2dd329773c71ace
Andres <ludena.wilson@outlook.com>
2019-11-18 09:46:05 +0000

    commit

--- src/main/java/com/sparta/mjn/beans/ActorBean.java
+++ src/main/java/com/sparta/mjn/beans/ActorBean.java
@@ -1,17 +1,11 @@
 package com.sparta.mjn.beans;
 
 import com.sparta.mjn.entities.Actor;
-import com.sparta.mjn.entities.Film;
 import com.sparta.mjn.services.ActorService;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 @Named
