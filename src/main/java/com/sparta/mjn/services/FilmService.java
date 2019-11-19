5257bc71b42ea9eae50d46b9f0de44c1478fa5af
Horia Constantina <hconstantina@spartaglobal.com>
2019-11-19 09:16:58 +0000

    First commit of the day

--- src/main/java/com/sparta/mjn/services/FilmService.java
+++ src/main/java/com/sparta/mjn/services/FilmService.java
@@ -25,11 +25,11 @@ public class FilmService {
     }
 
 
-//    @ManyToMany
-//    @JoinTable(
-//            name = "film",
-//            joinColumns = @JoinColumn(name = "film_id"),
-//            inverseJoinColumns = @JoinColumn(name = "actor_id"))
-//    Set<Film> filmCast;
+    @ManyToMany
+    @JoinTable(
+            name = "film",
+            joinColumns = @JoinColumn(name = "film_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id"))
+    Set<Film> filmCast;
 
 }
