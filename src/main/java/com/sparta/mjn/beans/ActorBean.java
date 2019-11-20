bd0c011927f3f66347e4562a2476ec104dadd157
Horia Constantina <hconstantina@spartaglobal.com>
2019-11-20 11:24:07 +0000

    ActorBean updated

--- src/main/java/com/sparta/mjn/beans/ActorBean.java
+++ src/main/java/com/sparta/mjn/beans/ActorBean.java
@@ -1,28 +1,28 @@
 package com.sparta.mjn.beans;
-
 import com.sparta.mjn.entities.Actor;
+import com.sparta.mjn.entities.Film;
 import com.sparta.mjn.services.ActorService;
-
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
-import java.util.Set;
 
 @Named
 @RequestScoped
-public class ActorBean {
-
+public class ActorBean{
     @Inject
     private ActorService actorService;
-
-    private Actor actor = new Actor();
-
-    public List getActorList(){
+    private int entityActorId;
+    private Actor actorEntity;
+    public List<Actor> actorList(){
         return actorService.getActorList();
     }
-
-    public Set getFilmList() {
-        return actorService.filmList(actor);
+    public String showActorFilmListPage(int actorId){
+        entityActorId = actorId;
+        actorEntity = actorService.getActorByID(entityActorId);
+        return "actorFilmsPage";
+    }
+    public List<Film> films(){
+        return actorService.getFilms(entityActorId);
     }
 }
