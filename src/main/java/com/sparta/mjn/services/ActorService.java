2363b0f1a1ebc80932dda853395484ea44425202
Horia Constantina <hconstantina@spartaglobal.com>
2019-11-20 11:25:38 +0000

    ActorService updated

--- src/main/java/com/sparta/mjn/services/ActorService.java
+++ src/main/java/com/sparta/mjn/services/ActorService.java
@@ -1,24 +1,22 @@
 package com.sparta.mjn.services;
-
 import com.sparta.mjn.entities.Actor;
 import com.sparta.mjn.entities.Film;
-
 import javax.ejb.Stateful;
 import javax.persistence.*;
 import java.util.List;
-import java.util.Set;
-
 @Stateful
 public class ActorService {
-
     @PersistenceContext(unitName = "UserPersistenceUnit")
     private EntityManager em;
-
-    public List getActorList(){
-        return em.createNamedQuery("getActors", Actor.class).getResultList();
+    public List <Actor> getActorList() {
+        return em.createNamedQuery("getActors",Actor.class).getResultList();
     }
-
-    public Set<Film> filmList(Actor actor) {
-        return actor.getFilmList();
+    public List<Film> getFilms(int actorId)
+    {
+        Query query = em.createQuery("SELECT a.films FROM Actor a WHERE a.actorID = :aI");
+        return query.setParameter("aI",actorId).getResultList();
+    }
+    public Actor getActorByID(int actorId){
+        return em.find(Actor.class,actorId);
     }
 }
